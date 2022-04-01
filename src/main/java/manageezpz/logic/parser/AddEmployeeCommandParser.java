2dc5508a650952072827c299119b432b4a8c2899
aaron-ljx <70706659+aaron-ljx@users.noreply.github.com>
2022-04-01 21:14:42 +0800

    ...

--- src/main/java/manageezpz/logic/parser/AddEmployeeCommandParser.java
+++ src/main/java/manageezpz/logic/parser/AddEmployeeCommandParser.java
@@ -49,5 +49,4 @@ public class AddEmployeeCommandParser implements Parser<AddEmployeeCommand> {
     private static boolean arePrefixesPresent(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
         return Stream.of(prefixes).allMatch(prefix -> argumentMultimap.getValue(prefix).isPresent());
     }
-
 }
